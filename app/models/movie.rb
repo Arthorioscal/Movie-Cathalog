dcc75ddb90302ef32154b509911ea15e087df562
ArthurScortegagna <arthurscortegagna@gmail.com>
2024-04-03 00:51:35 -0300

    Released e Unreleased adicionados

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
   belongs_to :director
   belongs_to :genre
+
+  enum status: { unreleased: 0, released: 1}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_28_144246) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_03_031924) do
   create_table "directors", force: :cascade do |t|
     t.string "name"
     t.string "nationality"
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_28_144246) do
     t.integer "genre_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["director_id"], name: "index_movies_on_director_id"
     t.index ["genre_id"], name: "index_movies_on_genre_id"
   end
